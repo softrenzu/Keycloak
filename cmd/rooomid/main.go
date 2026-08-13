a9f561eb99d2306a11b1e492ba7d03b0d0fb6649
softrenzu <softrenzu@yahoo.co.jp>
2026-08-14 01:41:53 +0900

    add RooomID entrypoint

---
+++ cmd/rooomid/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/softrenzu/Keycloak/internal/server"
+)
+
+func main() {
+	addr := os.Getenv("ROOOMID_ADDR")
+	if addr == "" {
+		addr = ":8080"
+	}
+	s := server.New()
+	srv := &http.Server{
+		Addr: addr,
+		Handler: s.Handler(),
+		ReadHeaderTimeout: 5 * time.Second,
+		ReadTimeout: 15 * time.Second,
+		WriteTimeout: 15 * time.Second,
+		IdleTimeout: 60 * time.Second,
+	}
+	log.Printf("RooomID listening on %s", addr)
+	log.Fatal(srv.ListenAndServe())
+}
